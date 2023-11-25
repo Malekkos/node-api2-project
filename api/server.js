48f5636ca04de1e554091e5ca62e457360e1e428
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-25 14:02:49 -0800

    Created the Post thingy to get methods, created then for posts to return posts and a catch with the defined message.

--- api/server.js
+++ api/server.js
@@ -4,13 +4,25 @@ const express = require("express");
 
 const server = express()
 
+const Post = require("./posts/posts-model")
 // /api/posts will be seperated
 
 
 
 
 
-server.get // /api/posts
+server.get("/api/posts", (req, res) => {
+  // console.log(req)
+    Post.find()
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved"
+      })
+    })
+}) // /api/posts
 
 server.get // /api/posts/:id
 
