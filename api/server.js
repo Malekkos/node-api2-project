c2adeebc0a2a4c6eec96d775cd179dd0e26507f5
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-25 13:52:04 -0800

    Installed express, setup my server.listen in index.js. Server.js has the preliminary stuff to start creating my endpoints.

--- api/server.js
+++ api/server.js
@@ -1,2 +1,25 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+
+const server = express()
+
+// /api/posts will be seperated
+
+
+
+
+
+server.get // /api/posts
+
+server.get // /api/posts/:id
+
+server.post // /api/posts
+
+server.put // /api/posts/:id
+
+server.delete // /api/posts/:id
+
+server.get // /api/posts/:id/comments
+
+module.exports = server
