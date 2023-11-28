b5a0283dc32638350a99e1f6a4604dc3b2079481
Christian Anderson <tehninjawaffle@gmail.com>
2023-11-27 21:57:19 -0800

    Made an attempt, currently it does remove the post but the specified mesage does not print and does not return the deleted post in question

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -90,7 +90,26 @@ router.put("/:id", (req,res) => {
     
 })
 
-router.delete // /api/posts/:id
+router.delete("/:id", (req, res) => {
+  const id = req.params.id
+  console.log("this is the id ", id) // works
+  if(!id) {
+    res.status(404).json({
+      message: "The post with the specified ID does not exist"
+    })
+  } else {
+    Post.remove(id)
+    .then(post => {
+      console.log("this is the deleted Post", post)
+      res.status(204).json(post)
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "The post could not be removed"
+      })
+    })
+  }
+}) // /api/posts/:id
 
 router.get // /api/posts/:id/comments
 
